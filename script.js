637e1367aeb062862f29dfb4722fc3817908ccf3
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2023-12-19 14:02:31 +0530

    removed error check

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
   const places = Object.freeze({
     pune: {
       name: "Pune",
-      number: [8,6,2,3,0,8,6,0,8,6].join(""),
+      number: [8, 6, 2, 3, 0, 8, 6, 0, 8, 6].join(""),
     },
     nagpur: {
       name: "Nagpur",
-      number: [8,1,8,1,0,6,0,0,6,0].join(""),
+      number: [8, 1, 8, 1, 0, 6, 0, 0, 6, 0].join(""),
     },
     thane: {
-        name:"Thane",
-        number: [9,3,9,3,0,5,0,5,0,5].join("")
-    }
+      name: "Thane",
+      number: [9, 3, 9, 3, 0, 5, 0, 5, 0, 5].join(""),
+    },
   });
-console.log(places)
+  console.log(places);
   const ivfCenterBtn = document.querySelectorAll(".ivf-center-location");
 
   const formData = {
@@ -27,21 +27,19 @@ console.log(places)
       formData.center = ivfCenter.getAttribute("data-ivf-center");
     });
   });
-//919284149958
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    try {
-      formData.name = e.target[0].value;
-      formData.phoneNumber = e.target[1].value;
-      
-      let url = `https://wa.me/${[9,1,9,2,8,4,1,4,9,9,5,8].join("")}?text=
+    formData.name = e.target[0].value;
+    formData.phoneNumber = e.target[1].value;
+
+    let url = `https://wa.me/${[9, 1, 9, 2, 8, 4, 1, 4, 9, 9, 5, 8].join(
+      ""
+    )}?text=
       Name: ${formData.name}%0aPhone number: ${formData.phoneNumber}%0a
-      Center: ${formData.center}%0a%0aThis message sent from wa link`
-      console.log(formData);
-      window.open(url, '_blank').focus();
-    } catch (err) {
-      console.log(err);
-    }
+      Center: ${formData.center}%0a%0aThis message sent from wa link`;
+    console.log(formData);
+    window.open(url, "_blank").focus();
   };
 
   const form = document.querySelector("#contact-form");
